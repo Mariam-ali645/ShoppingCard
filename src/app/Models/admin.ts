57e8a8b3b67e00ced834e9e2d219a66792365cbe
unknown <mariama428@gmail.com>
2024-10-23 13:44:39 +0300

    create component

---
+++ src/app/Models/admin.ts
@@ -0,0 +1,13 @@
+export class Admin {
+    "id": number;
+    "name": string;
+    "phoneNumber": string;
+    "country": string;
+    "city": string;
+    "email": string;
+    "userName": string;
+    "password": string;
+    "role": string;
+
+
+}
---
+++ src/app/Models/customer.ts
@@ -0,0 +1,11 @@
+export class Customer {
+    "id": number;
+    "name": string;
+    "phoneNumber": string;
+    "country": string;
+    "city": string;
+    "email": string;
+    "userName": string;
+    "password": string;
+    "role": string;
+}
